8fb5f63a7248068286e92d30e5ea596248960c3b
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-20 15:37:16 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import people from './data'
+import { FaBeer } from "react-icons/fa";
+import { FaQuoteRight } from "react-icons/fa";
 
 
 
 const App = () => {
-  const [ index,setIndex ] = useState(0)
+  const [ index,setIndex ] = useState(2)
   const {name,job,image,text} = people[index]
 
   console.log(name);
   
 
-  return <h2>Reviews Starter</h2>;
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt="" className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight  />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
+  );
 };
 export default App;
